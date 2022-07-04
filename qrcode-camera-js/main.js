c94278abe1648b4ce3f8d7cc2d42fac5373fb66e
Lorca-Leonel <108485895+Lorca-Leonel@users.noreply.github.com>
2022-07-04 14:05:34 -0300

    Update main.js

--- qrcode-camera-js/main.js
+++ qrcode-camera-js/main.js
@@ -89,7 +89,7 @@ const parseResult = _text => {
     dni      = data[4].trim()
     fechaNac = data[6].trim()
 
-  } else if (data.length <= 20 ) {
+  } else if (data.length == 15) {
     // Formato anterior
     apellido = data[4].trim()
     nombre   = data[5].trim()
@@ -121,7 +121,8 @@ const parseResult = _text => {
     result = result.replace('%apellido%', apellido);
     result = result.replace('%sexo%', sexo);
     result = result.replace('%dni%', dni);
-    dresult = result.replace('%fechaNac%', fechaNac);
+    result = result.replace('%fechaNac%', fechaNac);
+
     resultP.innerHTML = result;
     resultP.classList.add('success');
     if( navigator.vibrate ) {
